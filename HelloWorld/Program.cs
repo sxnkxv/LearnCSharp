9a4fe1eb655f6523a053f72784c65b0d81018012
senkxv <senkxv@gmail.com>
2026-05-18 21:49:44 +0300

    Create "HelloWorld"

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}!");
+        }
+    }
+}
