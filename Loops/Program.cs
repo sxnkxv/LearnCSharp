760c1b040b00151b88b7215cdaea8e54e27db8fd
senkxv <senkxv@gmail.com>
2026-05-22 13:16:05 +0300

    Create TaskTwo

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -29,7 +29,22 @@
 
             Console.WriteLine($"Итоговая сумма составит: {totalSum}");
         }
+        public static void TaskTwo() 
+        {
+            /* 
+             * Напишите программу, которая выводит на консоль таблицу умножения 
+             */
 
-        static void Main(string[] args) {}
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args) { TaskTwo(); }
     }
 }
