359625ac8f21b245e598b230456bee92ffe0f963
senkxv <senkxv@gmail.com>
2026-06-01 23:01:07 +0300

    Create task zero

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,9 +2,75 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static public void TaskZero()
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * Задан следующий трехмерный массив:
+             * int[,,] mas = { 
+             *   { { 1, 2 },{ 3, 4 } }, 
+             *   { { 4, 5 }, { 6, 7 } }, 
+             *   { { 7, 8 }, { 9, 10 } }, 
+             *   { { 10, 11 }, { 12, 13 }}
+             * };
+             * С помощью циклов переберите все элементы этого массива и выведите их на консоль в следующем виде:
+             * {{{1 , 2} , {3 , 4}} , {{4 , 5} , {6 , 7}} , {{7 , 8}, {9 , 10}} , {{10 , 11} , {12 , 13}}}
+             */
+
+            int[,,] mas = { 
+                { 
+                    { 1, 2 }, 
+                    { 3, 4 } 
+                },
+                { 
+                    { 4, 5 }, 
+                    { 6, 7 } 
+                },
+                { 
+                    { 7, 8 }, 
+                    { 9, 10 } 
+                },
+                { 
+                    { 10, 11 }, 
+                    { 12, 13 } 
+                }
+            };
+
+            int x = mas.GetUpperBound(0); // возвращает индекс последнего элемента
+            int y = mas.GetUpperBound(1);
+            int z = mas.GetUpperBound(2);
+
+            Console.Write("{");
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j <= y; j++)
+                {
+                    Console.Write("{");
+                    for (int k = 0; k <= z; k++)
+                    {
+                        Console.Write(mas[i, j, k]);
+                        if (k < z) 
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                    Console.Write("}");
+
+                    if (j < y)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.Write("}");
+
+                if (i < x)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.Write("}");
         }
+
+        static void Main(string[] args) { TaskZero(); }
     }
 }
