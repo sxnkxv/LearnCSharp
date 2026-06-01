cf3ca4b1e4a155da1424002515aa8c11e857339d
senkxv <senkxv@gmail.com>
2026-06-01 21:22:33 +0300

    Create four task

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -70,7 +70,40 @@
 
             Console.WriteLine($"Ваш вклад составит: {depositAmount}");
         }
+        public static void TaskFour()
+        {
+            /* 
+             * Напишите программу, в которую пользователь вводит два числа и выводит результат их умножения. 
+             * При этом программа должны запрашивать у пользователя ввод чисел, пока оба вводимых числа не окажутся в диапазоне от 0 до 10. 
+             * Если введенные числа окажутся больше 10 или меньше 0, то программа должна вывести пользователю о том, 
+             * что введенные числа недопустимы, и повторно запросить у пользователя ввод двух чисел. 
+             * Если введенные числа принадлежат диапазону от 0 до 10, то программа выводит результат умножения.
+             * Для организации ввода чисел используйте бесконечный цикл while и оператор break.
+             */
 
-        static void Main(string[] args) { TaskTwo(); }
+            double firstNumber;
+            double secondNumber;
+
+            while (true)
+            {
+                Console.Write("Введите первое число: ");
+                firstNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                if ((firstNumber >= 0 && firstNumber <= 10) && (secondNumber >= 0 && secondNumber <= 10))
+                {
+                    Console.WriteLine($"Результат умножения: {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введенные числа недопустимы!");
+                }
+            }
+        }
+
+        static void Main(string[] args) { }
     }
 }
